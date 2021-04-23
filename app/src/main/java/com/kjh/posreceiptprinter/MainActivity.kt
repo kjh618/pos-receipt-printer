9f451cbe045d4ef2a6f9b6cf5ed096836620756c
Jihoon Kim <kjh618@kaist.ac.kr>
2021-04-23 19:21:52 +0900

    Implement USB connection

--- app/src/main/java/com/kjh/posreceiptprinter/MainActivity.kt
+++ app/src/main/java/com/kjh/posreceiptprinter/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.kjh.posreceiptprinter
 
+import android.content.Context
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+
+        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+        val infoText = findViewById<TextView>(R.id.textInfo)
+        infoText.text = device.toString()
+
+        val bytes = "test\n".toByteArray()
+
+        val intf = device?.getInterface(0)
+        val endpoint = intf?.getEndpoint(0)
+        val connection = manager.openDevice(device)
+        connection.claimInterface(intf, true)
+        connection.bulkTransfer(endpoint, bytes, bytes.size, 0)
     }
 }
