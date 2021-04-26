addbb1af0a0e0df0175199a429082a86077d919e
Jihoon Kim <kjh618@kaist.ac.kr>
2021-04-26 21:16:56 +0900

    Add ability to directly input bytes

--- app/src/main/java/com/kjh/posreceiptprinter/MainActivity.kt
+++ app/src/main/java/com/kjh/posreceiptprinter/MainActivity.kt
@@ -7,11 +7,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
-    lateinit var usbInterface: UsbInterface
-    lateinit var usbEndpoint: UsbEndpoint
-    lateinit var usbConnection: UsbDeviceConnection
+    private lateinit var usbInterface: UsbInterface
+    private lateinit var usbEndpoint: UsbEndpoint
+    private lateinit var usbConnection: UsbDeviceConnection
+
+    private var printContent: MutableList<Byte> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +23,10 @@ class MainActivity : AppCompatActivity() {
         val manager = getSystemService(Context.USB_SERVICE) as UsbManager
 
         val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
-        val infoText = findViewById<TextView>(R.id.textInfo)
-        infoText.text = device.toString()
+        val textViewInfo = findViewById<TextView>(R.id.textViewInfo)
+        textViewInfo.text = device.toString()
         if (device == null) {
-            finish()
+            finishAndRemoveTask()
             return
         }
 
@@ -32,10 +35,33 @@ class MainActivity : AppCompatActivity() {
         usbConnection = manager.openDevice(device)
     }
 
+    private fun updateTextViewPrintContent() {
+        val textViewPrintContent = findViewById<TextView>(R.id.textViewPrintContent)
+        textViewPrintContent.text = printContent.toString()
+    }
+
+    fun inputNumber(view: View) {
+        val editTextNumberInput = findViewById<EditText>(R.id.editTextNumberInput)
+        val byte = editTextNumberInput.text.toString().toByte()
+        editTextNumberInput.setText("")
+        printContent.add(byte)
+        updateTextViewPrintContent()
+    }
+
+    fun delete(view: View) {
+        printContent.removeLastOrNull()
+        updateTextViewPrintContent()
+    }
+
+    fun inputString(view: View) {
+        val editTextStringInput = findViewById<EditText>(R.id.editTextStringInput)
+        val bytes = editTextStringInput.text.toString().toByteArray(Charset.forName("EUC-KR"))
+        bytes.toCollection(printContent)
+        updateTextViewPrintContent()
+    }
+
     fun print(view: View) {
-        val editTextContent = findViewById<EditText>(R.id.editTextPrintContent)
-        val bytes = editTextContent.text.toString().toByteArray()
         usbConnection.claimInterface(usbInterface, true)
-        usbConnection.bulkTransfer(usbEndpoint, bytes, bytes.size, 0)
+        usbConnection.bulkTransfer(usbEndpoint, printContent.toByteArray(), printContent.size, 0)
     }
 }
